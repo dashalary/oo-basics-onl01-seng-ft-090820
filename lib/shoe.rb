7f29446b53254efb91f27fc312836eb2abb184f0
Dasha Lary <dk2800@nyu.edu>
2020-09-14 18:00:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,15 @@
 class Shoe
-  
+   attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+ 
+  def cobble
+    puts "Flipping the page...wow, you read fast!"
+  end
   
   
   
