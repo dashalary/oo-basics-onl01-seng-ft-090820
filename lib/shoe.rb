0a7732ecdc3b08309bffb4c32ed2686d6690ba6d
Dasha Lary <dk2800@nyu.edu>
2020-09-14 18:04:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,10 @@ class Shoe
    attr_accessor :color, :size, :material, :condition
  attr_reader :brand
  
- 
+  def initialize(brand)
+    @brand = brand
+  end
+  
   def cobble
     puts "Flipping the page...wow, you read fast!"
   end
